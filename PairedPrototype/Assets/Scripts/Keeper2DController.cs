82b42edfa5611c401e08caf60f52163899bde161
omid-hd <omid@aldora.io>
2026-02-08 01:04:13 -0800

    Keeper moves up and down Script

--- PairedPrototype/Assets/Scripts/Keeper2DController.cs
+++ PairedPrototype/Assets/Scripts/Keeper2DController.cs
@@ -4,6 +4,8 @@ public class Keeper2DController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed = 1.0f;
+    public float top = 6f;
+    public float bottom = 1f;
     void Start()
     {
         
@@ -12,6 +14,8 @@ public class Keeper2DController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * Time.deltaTime * speed);
+        float distance = Mathf.Abs(top-bottom);
+        float back_and_forth = Mathf.PingPong(Time.time * speed, distance) + bottom;
+        transform.position = new Vector3(transform.position.x, back_and_forth, transform.position.z);
     }
 }
